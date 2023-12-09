bc221b5cfecaf04dd0664dbcca91549c716ec2ad
Ash Griffiths <46426454+AshGriffiths@users.noreply.github.com>
2023-12-09 06:49:17 +0000

    Seemed too easy

---
+++ 2023/day_nine/day9.py
@@ -0,0 +1,18 @@
+from itertools import pairwise
+
+
+def next_reading(reading_set):
+    if len(set(reading_set)) == 1:
+        return reading_set[0]
+    return reading_set[-1] + next_reading([x[1] - x[0] for x in pairwise(reading_set)])
+
+
+with open("input.txt", "r") as input:
+    readings = [[int(val) for val in line.split(" ")] for line in input.readlines()]
+    p1_results = []
+    p2_results = []
+    for reading_set in readings:
+        p1_results.append(next_reading(reading_set))
+        p2_results.append(next_reading(reading_set[::-1]))
+    print(f"Part One : {sum(p1_results)}")
+    print(f"Part Two : {sum(p2_results)}")
