35d6eb33b69cfbf05e356d13d9c3e803ec1b189c
Ash Griffiths <46426454+AshGriffiths@users.noreply.github.com>
2023-12-12 08:19:39 +0000

    corrections

--- 2023/day_twelve/day12.py
+++ 2023/day_twelve/day12.py
@@ -2,7 +2,7 @@ from functools import cache
 
 
 @cache
-def count_matches(springs: str, groups: tuple[int, ...]) -> int:
+def count_possibilities(springs: str, groups: tuple[int, ...]) -> int:
     if not groups:
         if all(spring in [".", "?"] for spring in springs):
             return 1
@@ -18,7 +18,7 @@ def count_matches(springs: str, groups: tuple[int, ...]) -> int:
             spring == possible_spring or spring == "?"
             for spring, possible_spring in zip(springs, possible_springs)
         ):
-            count += count_matches(springs[len(possible_springs) :], cdr_ints)
+            count += count_possibilities(springs[len(possible_springs) :], cdr_ints)
     return count
 
 
@@ -34,7 +34,7 @@ with open("input.txt", "r") as input:
         int_groups_p1 = tuple(map(int, groups.split(",")))
         int_groups_p2 = int_groups_p1 * 5
         print(int_groups_p2)
-        total_p1 += count_matches(pattern, int_groups_p1)
-        total_p2 += count_matches("?".join((pattern,) * 5), int_groups_p2)
+        total_p1 += count_possibilities(pattern, int_groups_p1)
+        total_p2 += count_possibilities("?".join((pattern,) * 5), int_groups_p2)
     print(f"Part One : {total_p1}")
     print(f"Part Two : {total_p2}")
